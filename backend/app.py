13f81fa62c046fe7a734ffb83f58af5ba2670e0d
Sujitha <sujithalatheesh1415@gmail.com>
2024-04-03 21:18:27 +0800

    map4

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import json
+import pandas as pd
+from datetime import datetime, date
 
 app = Flask(__name__, template_folder='../frontend/templates')
 
@@ -14,39 +16,35 @@ def about():
 
 @app.route('/map')
 def map():
-    with open('../data/data_sample.json', 'r') as f:
+    with open('../data/output_file.json', 'r') as f:
       fileData = json.load(f)
       f.close()
+    years = request.args.get('years', default=None)
     town = request.args.get('town', default=None)
-    
     storey_range = request.args.get('storeyRange', default=None)
     flat_model = request.args.get('flatModel', default=None)
     filtered_data = [item for item in fileData if
                      (not town or item['town'] == town) and
                      (not storey_range or item['storey_range'] == storey_range) and
-                     (not flat_model or item['flat_model'] == flat_model)]
-    towns = [
-        'ANG MO KIO', 'BEDOK', 'BISHAN', 'BUKIT BATOK', 'BUKIT MERAH', 'BUKIT PANJANG',
-        'BUKIT TIMAH', 'CENTRAL AREA', 'CHOA CHU KANG', 'CLEMENTI', 'GEYLANG',
-        'HOUGANG', 'JURONG EAST', 'JURONG WEST', 'KALLANG/WHAMPOA', 'MARINE PARADE',
-        'PASIR RIS', 'PUNGGOL', 'QUEENSTOWN', 'SEMBAWANG', 'SENGKANG', 'SERANGOON',
-        'TAMPINES', 'TOA PAYOH', 'WOODLANDS', 'YISHUN', 'CLEME'
-    ]
+                     (not flat_model or item['flat_model'] == flat_model) ]
+    towns = ['', 'ANG MO KIO', 'BEDOK', 'BISHAN', 'BUKIT BATOK', 'BUKIT MERAH',
+       'BUKIT PANJANG', 'BUKIT TIMAH', 'CENTRAL AREA', 'CHOA CHU KANG',
+       'CLEMENTI', 'GEYLANG', 'HOUGANG', 'JURONG EAST', 'JURONG WEST',
+       'KALLANG/WHAMPOA', 'MARINE PARADE', 'PASIR RIS', 'PUNGGOL',
+       'QUEENSTOWN', 'SEMBAWANG', 'SENGKANG', 'SERANGOON', 'TAMPINES',
+       'TOA PAYOH', 'WOODLANDS', 'YISHUN']
     
-    storey_ranges = [
-        '01 TO 03', '04 TO 06', '07 TO 09', '10 TO 12', '13 TO 15', '16 TO 18',
-        '19 TO 21', '22 TO 24', '25 TO 27', '28 TO 30', '31 TO 33', '34 TO 36',
-        '37 TO 39', '40 TO 42', '43 TO 45', '46 TO 48', '49 TO 51'
-    ]
+    storey_ranges =['','10 TO 12', '01 TO 03', '04 TO 06', '07 TO 09', '13 TO 15',
+       '19 TO 21', '22 TO 24', '16 TO 18', '34 TO 36', '28 TO 30',
+       '37 TO 39', '49 TO 51', '25 TO 27', '40 TO 42', '31 TO 33']
     
-    flat_models = [
-        '2-room', '3Gen', 'Adjoined flat', 'Apartment', 'DBSS', 'Improved',
-        'Improved-Maisonette', 'Maisonette', 'Model A', 'Model A-Maisonette', 'Model A2',
-        'Multi Generation', 'New Generation', 'Premium Apartment', 'Premium Apartment Loft',
-        'Premium Maisonette', 'Simplified', 'Standard', 'Terrace', 'Type S1', 'Type S2'
-    ]
+    flat_models = ['','Improved', 'New Generation', 'DBSS', 'Standard', 'Apartment',
+       'Simplified', 'Model A', 'Premium Apartment', 'Adjoined flat',
+       'Model A-Maisonette', 'Maisonette', 'Type S1', 'Type S2',
+       'Model A2']
+    year = [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024]
     return render_template('map.html', fileData=filtered_data, towns=towns, storey_ranges=storey_ranges, flat_models=flat_models, 
-                           town = town, storey_range =storey_range, flat_model=flat_model )
+                           town = town, storey_range =storey_range, flat_model=flat_model, year = year )
 # Example of handling form submission
 @app.route('/submit', methods=['POST'])
 def submit():
